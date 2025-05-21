41132f92617c4b125be461f28cc9dac27d775c0d
esraodaci22 <esra.odaci.22@gmail.com>
2025-05-21 20:36:38 +0300

    add splash

---
+++ app/src/main/java/com/srdc/plantapp/ui/ui/splash/SplashFragment.kt
@@ -0,0 +1,24 @@
+package com.srdc.plantapp.ui.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.plantapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
